e1685a5e1ef4621e90588c44c51b00da55905318
Ssukdaebat60 <84669895+Ssukdaebat60@users.noreply.github.com>
2021-12-23 14:28:21 +0900

    using getter

--- RGBColorFilter.cpp
+++ RGBColorFilter.cpp
@@ -1,18 +1,13 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <Windows.h> 
 #include "BmpProcess.h"
-
-extern BITMAPINFOHEADER hInfo;
 
 int main(){
 	openBMP("sample1.bmp");
-	setXY(0, 0, hInfo.biWidth/2, hInfo.biHeight/2);
-	multipleBGR(1, 0, 0, true);
-	setXY(hInfo.biWidth/2, hInfo.biHeight/2, hInfo.biWidth, hInfo.biHeight);
-	multipleBGR(0, 1, 0, true);
-	setXY(0, hInfo.biHeight/2, hInfo.biWidth/2, hInfo.biHeight);
-	multipleBGR(0, 0, 1, true);
+	setXY(0, 0, getWidth()/2, getHeight()/2);
+	multipleBGR(1, 0, 0);
+	setXY(getWidth()/2, getHeight()/2, getWidth(), getHeight());
+	multipleBGR(0, 1, 0);
+	setXY(0, getHeight()/2, getWidth()/2, getHeight());
+	multipleBGR(0, 0, 1);
 	writeBMP("output.bmp");
 	closeBGR();
 	return 0;
